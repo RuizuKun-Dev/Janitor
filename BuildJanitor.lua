670da2c08d4a5c2f9ea28d3724df960140b04777
RuizuKun_Dev <55673217+RuizuKun-Dev@users.noreply.github.com>
2022-04-19 05:06:59 +0700

    style (stylua): 🎨

--- BuildJanitor.lua
+++ BuildJanitor.lua
@@ -1,8 +1,8 @@
-local ffi = require('ffi')
+local ffi = require("ffi")
 
-ffi.cdef'\r\n\tint printf(const char * Format, ...);\r\n'
-os.execute(string.format('rojo build -o Janitor.rbxm default.project.json'))
-os.execute(string.format('rojo build -o Janitor.rbxmx default.project.json'))
-ffi.C.printf('Built Janitor!\n')
+ffi.cdef("\r\n\tint printf(const char * Format, ...);\r\n")
+os.execute(string.format("rojo build -o Janitor.rbxm default.project.json"))
+os.execute(string.format("rojo build -o Janitor.rbxmx default.project.json"))
+ffi.C.printf("Built Janitor!\n")
 
 return 1
--- src/RbxScriptConnection.lua
+++ src/RbxScriptConnection.lua
@@ -4,17 +4,17 @@ RbxScriptConnection.Connected = true
 RbxScriptConnection.__index = RbxScriptConnection
 
 function RbxScriptConnection:Disconnect()
-    if self.Connected then
-        self.Connected = false
+	if self.Connected then
+		self.Connected = false
 
-        self.Connection:Disconnect()
-    end
+		self.Connection:Disconnect()
+	end
 end
 function RbxScriptConnection._new(RBXScriptConnection)
-    return setmetatable({Connection = RBXScriptConnection}, RbxScriptConnection)
+	return setmetatable({ Connection = RBXScriptConnection }, RbxScriptConnection)
 end
 function RbxScriptConnection:__tostring()
-    return'RbxScriptConnection<' .. tostring(self.Connected) .. '>'
+	return "RbxScriptConnection<" .. tostring(self.Connected) .. ">"
 end
 
 return RbxScriptConnection
