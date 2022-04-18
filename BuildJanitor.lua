4e53cc9528d3fdcfb2e4d5c15ba4ef4a2a341d22
RuizuKun_Dev <55673217+RuizuKun-Dev@users.noreply.github.com>
2022-04-19 05:06:30 +0700

    style (darklua): 🎨

--- BuildJanitor.lua
+++ BuildJanitor.lua
@@ -1,11 +1,8 @@
-local ffi = require("ffi")
+local ffi = require('ffi')
 
--- stylua: ignore
-ffi.cdef[[
-	int printf(const char * Format, ...);
-]]
+ffi.cdef'\r\n\tint printf(const char * Format, ...);\r\n'
+os.execute(string.format('rojo build -o Janitor.rbxm default.project.json'))
+os.execute(string.format('rojo build -o Janitor.rbxmx default.project.json'))
+ffi.C.printf('Built Janitor!\n')
 
-os.execute(string.format("rojo build -o Janitor.rbxm default.project.json"))
-os.execute(string.format("rojo build -o Janitor.rbxmx default.project.json"))
-ffi.C.printf("Built Janitor!\n")
 return 1
